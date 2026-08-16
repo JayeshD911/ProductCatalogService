329cf1ae6e5d5e8db36d3c04d5c3b227ccc67849
Jayesh <jayeshdongre31@gmail.com>
2026-08-16 18:03:27 +0530

    Add Product to DTO convert function

--- src/main/java/io/github/jayeshd911/productcatalogservice/models/Product.java
+++ src/main/java/io/github/jayeshd911/productcatalogservice/models/Product.java
@@ -1,5 +1,7 @@
 package io.github.jayeshd911.productcatalogservice.models;
 
+import io.github.jayeshd911.productcatalogservice.dtos.CategoryDTO;
+import io.github.jayeshd911.productcatalogservice.dtos.ProductDTO;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,4 +13,21 @@ public class Product extends BaseModel {
     private Double price;
     private String imageUrl;
     private Category category;
+
+    public ProductDTO convert() {
+        ProductDTO dto = new ProductDTO();
+        dto.setId(this.getId());
+        dto.setName(this.getName());
+        dto.setDescription(this.getDescription());
+        dto.setPrice(this.getPrice());
+        dto.setImageUrl(this.getImageUrl());
+        if (this.getCategory() != null) {
+            CategoryDTO categoryDTO = new CategoryDTO();
+            categoryDTO.setId(this.getCategory().getId());
+            categoryDTO.setName(this.getCategory().getName());
+            categoryDTO.setDescription(this.getCategory().getDescription());
+            dto.setCategory(categoryDTO);
+        }
+        return dto;
+    }
 }
