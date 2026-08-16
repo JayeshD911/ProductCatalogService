dbf31734eeb125cc9a8b80225ceeda5b765c7802
Jayesh <jayeshdongre31@gmail.com>
2026-08-16 18:08:26 +0530

    implement getProductById in ProductController

--- src/main/java/io/github/jayeshd911/productcatalogservice/controllers/ProductController.java
+++ src/main/java/io/github/jayeshd911/productcatalogservice/controllers/ProductController.java
@@ -1,7 +1,10 @@
 package io.github.jayeshd911.productcatalogservice.controllers;
 
-import io.github.jayeshd911.productcatalogservice.dtos.ProductRequestDTO;
-import io.github.jayeshd911.productcatalogservice.dtos.ProductResponseDTO;
+import io.github.jayeshd911.productcatalogservice.dtos.ProductDTO;
+import io.github.jayeshd911.productcatalogservice.models.Product;
+import io.github.jayeshd911.productcatalogservice.services.IProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,21 +13,42 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    IProductService productService;
+
+    // Constructor injection
+    public ProductController(IProductService productService) {
+        this.productService = productService;
+    }
+
     @PostMapping("/products")
-    ProductResponseDTO createProduct(@RequestBody ProductRequestDTO productRequestDTO) {
-        ProductResponseDTO ProductResponseDTO = new ProductResponseDTO();
+    ProductDTO createProduct(@RequestBody ProductDTO productRequestDTO) {
+        ProductDTO ProductResponseDTO = new ProductDTO();
+
         return ProductResponseDTO;
     }
 
     @GetMapping("/products/{id}")
-    ProductResponseDTO getProductById(@PathVariable("id") Long id) {
-        ProductResponseDTO ProductResponseDTO = new ProductResponseDTO();
-        return ProductResponseDTO;
+    ResponseEntity<ProductDTO> getProductById(@PathVariable("id") Long id) {
+
+        if(id == null || id <= 1) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        ProductDTO productDTO = new ProductDTO();
+        Product product = productService.getProductById(id);
+
+        if(product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        productDTO = product.convert();
+
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
     }
 
     @GetMapping("/products")
-    List<ProductResponseDTO> getAllProducts() {
-        List<ProductResponseDTO> products = new ArrayList<>();
+    List<ProductDTO> getAllProducts() {
+        List<ProductDTO> products = new ArrayList<>();
         return products;
     }
 
