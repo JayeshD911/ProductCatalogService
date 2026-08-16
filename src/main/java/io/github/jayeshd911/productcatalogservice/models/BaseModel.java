00ddf918d2f4e249a73c0db840791be88918a40b
Jayesh <jayeshdongre31@gmail.com>
2026-08-16 17:51:01 +0530

    Add annotations

--- src/main/java/io/github/jayeshd911/productcatalogservice/models/BaseModel.java
+++ src/main/java/io/github/jayeshd911/productcatalogservice/models/BaseModel.java
@@ -1,7 +1,12 @@
 package io.github.jayeshd911.productcatalogservice.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public abstract class BaseModel {
     private Long id;
     private Date createdAt;
