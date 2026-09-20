7bbd7719ee13cd1d5c9c6faf7d944a8c01b94783
Jayesh <jayeshdongre31@gmail.com>
2026-09-20 23:50:27 +0530

    implement convertToProduct

--- src/main/java/io/github/jayeshd911/productcatalogservice/dtos/ProductDTO.java
+++ src/main/java/io/github/jayeshd911/productcatalogservice/dtos/ProductDTO.java
@@ -1,5 +1,6 @@
 package io.github.jayeshd911.productcatalogservice.dtos;
 
+import io.github.jayeshd911.productcatalogservice.models.Product;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,4 +15,20 @@ public class ProductDTO {
     private String imageUrl;
 
 
+    public Product convertToProduct(){
+        Product product = new Product();
+        product.setId(this.getId());
+        product.setName(this.getName());
+        product.setDescription(this.getDescription());
+        product.setPrice(this.getPrice());
+        product.setImageUrl(this.getImageUrl());
+        if (this.getCategory() != null) {
+            CategoryDTO categoryDTO = new CategoryDTO();
+            categoryDTO.setId(this.getCategory().getId());
+            categoryDTO.setName(this.getCategory().getName());
+            categoryDTO.setDescription(this.getCategory().getDescription());
+            product.setCategory(CategoryDTO.convert());
+        }
+        return product;
+    }
 }
