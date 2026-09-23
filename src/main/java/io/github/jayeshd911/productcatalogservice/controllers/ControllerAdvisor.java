90ece6b608f36fdd13f3ad1f5dc12e9e59146eb4
Jayesh <jayeshdongre31@gmail.com>
2026-09-23 16:03:44 +0530

    implement handleIllegalArgumentException

---
+++ src/main/java/io/github/jayeshd911/productcatalogservice/controllers/ControllerAdvisor.java
@@ -0,0 +1,14 @@
+package io.github.jayeshd911.productcatalogservice.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvisor {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
