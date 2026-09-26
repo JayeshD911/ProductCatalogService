f679250c987aae09096fb38ebfe4ab0989b112d5
Jayesh <jayeshdongre31@gmail.com>
2026-09-26 17:18:55 +0530

    add Category entity mapping and product relationship

--- src/main/java/io/github/jayeshd911/productcatalogservice/models/Category.java
+++ src/main/java/io/github/jayeshd911/productcatalogservice/models/Category.java
@@ -1,5 +1,7 @@
 package io.github.jayeshd911.productcatalogservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,9 +9,11 @@ import java.util.List;
 
 @Getter
 @Setter
+@Entity
 public class Category extends BaseModel {
     private String name;
     private String description;
+    @OneToMany(mappedBy = "category") // This indicates that the "category" field in the Product entity is the owner of the relationship
     private List<Product> products;
 
 }
