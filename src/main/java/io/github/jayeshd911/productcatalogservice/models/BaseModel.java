f7c266fa75e83ba93826231d30bba6122c99b67b
Jayesh <jayeshdongre31@gmail.com>
2026-09-26 17:16:38 +0530

    remove application.properties

--- src/main/java/io/github/jayeshd911/productcatalogservice/models/BaseModel.java
+++ src/main/java/io/github/jayeshd911/productcatalogservice/models/BaseModel.java
@@ -1,13 +1,18 @@
 package io.github.jayeshd911.productcatalogservice.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+
 
 import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class BaseModel {
+    @Id
     private Long id;
     private Date createdAt;
     private Date updatedAt;
