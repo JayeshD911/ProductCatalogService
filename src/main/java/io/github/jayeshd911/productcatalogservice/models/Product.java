3aad5f4dc6b7c47818c2c4c290ae6bee8753a59c
Jayesh <jayeshdongre31@gmail.com>
2026-09-26 17:19:08 +0530

    add Product entity mapping and product relationship

--- src/main/java/io/github/jayeshd911/productcatalogservice/models/Product.java
+++ src/main/java/io/github/jayeshd911/productcatalogservice/models/Product.java
@@ -3,16 +3,21 @@ package io.github.jayeshd911.productcatalogservice.models;
 import io.github.jayeshd911.productcatalogservice.dtos.CategoryDTO;
 import io.github.jayeshd911.productcatalogservice.dtos.FakestoreProductDTO;
 import io.github.jayeshd911.productcatalogservice.dtos.ProductDTO;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     private String name;
     private String description;
     private Double price;
     private String imageUrl;
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
 
     public FakestoreProductDTO convertTofakestoreProductDTO() {
